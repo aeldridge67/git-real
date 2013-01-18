3f28718e3c05a9f274466ac83beec3b41c2cb162
Allan Eldridge <allan@nancyandallan.com>
2013-01-18 13:48:45 -0500

    Add PHP file

---
+++ my.php
@@ -0,0 +1,88 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Basic Tipsy Tests</title>
+    
+<!--         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script> -->
+<!-- 		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script> -->
+		<script type="text/javascript" src="jquery-1.7.2.js"></script>
+		<script type="text/javascript" src="jquery.tipsy.js"></script>
+		<script type="text/javascript" src="my.js"></script>
+		<script type="text/javascript" src="fixed-floating-elements.js"></script>
+		<link rel="stylesheet" href="tipsy.css">
+		<link rel="stylesheet" href="fixed-floating-elements.css">
+</head>
+<body>
+
+<p>
+<a id="example1" href="#" title="Why Hello World">Hover over me</a>
+</p>
+
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+
+
+<div id="comments">
+  <ol>
+    <li>Here be the comments from visitors...</li>
+    <li>etc...</li>
+  </ol>
+</div>
+
+<div id="commentWrapper">
+  <div id="comment">
+<form>
+<div id="example2">
+<input type="checkbox" id="example3" title="Why Hello Checkbox">EXAMPLE 2</div>
+
+</form>
+  </div>
+</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+<div id="example4">EXAMPLE 4<BR>EXAMPLE 4</div>
+
+
+
+
+</body>
+</html>
